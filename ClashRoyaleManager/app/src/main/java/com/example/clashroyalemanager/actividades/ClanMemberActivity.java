034c3f953593aa5add71a256e4cce83b8ed5c29c
Joan M.H <73333828+JMayHub@users.noreply.github.com>
2023-10-25 13:31:58 +0200

    Version 0.1.1
    Interface changes and uprgades:
    
    Main screen colors changes depending of the light or dark mode.
    
    Member Clan screen information location fixed. Now the information is displayed correctly where it should be.

--- ClashRoyaleManager/app/src/main/java/com/example/clashroyalemanager/actividades/ClanMemberActivity.java
+++ ClashRoyaleManager/app/src/main/java/com/example/clashroyalemanager/actividades/ClanMemberActivity.java
@@ -3,13 +3,13 @@ package com.example.clashroyalemanager.actividades;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.clashroyalemanager.R;
 import com.example.clashroyalemanager.api.RoyaleAPI;
 import com.example.clashroyalemanager.clases.Player;
+import com.squareup.picasso.Picasso;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -18,8 +18,12 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ClanMemberActivity extends AppCompatActivity {
-    private ImageView ivClanMemberProfile;
+    private ImageView ivClanMemberFavCard;
     private TextView tvClanMemberName;
+    private TextView tvClanName;
+    private TextView tvClanRole;
+    private TextView tvDonations;
+    private TextView tvWarDayWins;
     private String id;
 
     @Override
@@ -27,14 +31,17 @@ public class ClanMemberActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clan_member);
 
-        ivClanMemberProfile = findViewById(R.id.ivClanMemberProfile);
+        ivClanMemberFavCard = findViewById(R.id.ivClanMemberFavCard);
         tvClanMemberName = findViewById(R.id.tvClanMemberName);
+        tvClanName = findViewById(R.id.tvClanName);
+        tvClanRole = findViewById(R.id.tvClanRole);
+        tvDonations = findViewById(R.id.tvDonations);
+        tvWarDayWins = findViewById(R.id.tvWarDayWins);
 
         //Obtiene el id del actor que se a seleccionado en la anterior actividad.
         Bundle extras = getIntent().getExtras();
         if (extras !=null) {
             id = extras.getString("id").substring(1);
-            Toast.makeText(this, id, Toast.LENGTH_LONG).show();
         }
 
         //TODO: (7) Creo una instancia de la clase Retrofit, a la que le indico la URL de la API
@@ -56,18 +63,26 @@ public class ClanMemberActivity extends AppCompatActivity {
             public void onResponse(Call<Player> call, Response<Player> response) {
                 switch(response.code()) {
                     case 200:
-                        Toast.makeText(getBaseContext(), "200", Toast.LENGTH_LONG).show();
                         //TODO: (11) En caso de que haya éxito en la respuesta, extraigo la información del jugador
                         Player player = response.body();
 
                         String name = player.getName();
+                        String clanName = player.getClan().getName();
+                        String clanRole = player.getRole();
+                        String donations = player.getDonations().toString();
+                        String warDayWins = player.getWarDayWins().toString();
+                        String path = player.getCurrentFavouriteCard().getIconUrls().getMedium();
 
                         tvClanMemberName.setText(name);
+                        tvClanName.setText(clanName);
+                        tvClanRole.setText(clanRole);
+                        tvDonations.setText("Donations: " + donations);
+                        tvWarDayWins.setText("War Day Wins: " + warDayWins);
+                        Picasso.get().load(path).into(ivClanMemberFavCard);
                         break;
                     case 401:
                         break;
                     default:
-                        Toast.makeText(getBaseContext(), response.toString(), Toast.LENGTH_LONG).show();
                         break;
                 }
             }
--- ClashRoyaleManager/app/src/main/java/com/example/clashroyalemanager/api/RoyaleAPI.java
+++ ClashRoyaleManager/app/src/main/java/com/example/clashroyalemanager/api/RoyaleAPI.java
@@ -16,24 +16,16 @@ public interface RoyaleAPI {
     @Headers({
             "Content-Type: application/json; charset=utf-8",
             "Host: proxy.royaleapi.dev",
-            "Access-Control-Allow-Origin: *",
-            "Access-Control-Allow-Methods: GET, POST, OPTIONS",
-            "Access-Control-Allow-Headers: DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range,Azuthorization",
-            "Access-Control-Expose-Headers: Content-Length,Content-Range,Authorization",
             "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImRmNjA3OTc4LTE1YTMtNDA5Yi05NWQwLTI5OWE2M2JkMTg0NiIsImlhdCI6MTY3ODUyODU2OCwic3ViIjoiZGV2ZWxvcGVyL2RmZTYxNmRhLTE2ZGEtZTVhNy05ZTA5LWZhMTczZjI5N2RjNiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI0NS43OS4yMTguNzkiXSwidHlwZSI6ImNsaWVudCJ9XX0.uW2ApDK1wsAtvD-ImkvPjILVs2thX7bgOM0W58aVdrDUSZPSsM5kajMY81E-mmN264AZ2UIDovvM-mHB4jiAeQ"
     })
-    @GET("players/%23{id}")//9YG8UGLU
+    @GET("players/%23{id}")//#9YG8UGLU
     Call<Player> getPlayer(@Path("id") String id);
 
     @Headers({
             "Content-Type: application/json; charset=utf-8",
             "Host: proxy.royaleapi.dev",
-            "Access-Control-Allow-Origin: *",
-            "Access-Control-Allow-Methods: GET, POST, OPTIONS",
-            "Access-Control-Allow-Headers: DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range,Azuthorization",
-            "Access-Control-Expose-Headers: Content-Length,Content-Range,Authorization",
             "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImRmNjA3OTc4LTE1YTMtNDA5Yi05NWQwLTI5OWE2M2JkMTg0NiIsImlhdCI6MTY3ODUyODU2OCwic3ViIjoiZGV2ZWxvcGVyL2RmZTYxNmRhLTE2ZGEtZTVhNy05ZTA5LWZhMTczZjI5N2RjNiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI0NS43OS4yMTguNzkiXSwidHlwZSI6ImNsaWVudCJ9XX0.uW2ApDK1wsAtvD-ImkvPjILVs2thX7bgOM0W58aVdrDUSZPSsM5kajMY81E-mmN264AZ2UIDovvM-mHB4jiAeQ"
     })
-    @GET("clans/%23{id}/members")//2200JG8L
+    @GET("clans/%23{id}/members")//#2200JG8L
     Call<ClanMembers> getClanMembers(@Path("id") String id);
 }
